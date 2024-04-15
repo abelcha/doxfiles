309ee63f097d13cb9606c9e371055e7f4b74f36a
Abel Chalier <abel.chalier@gmail.com>
2024-04-15 02:39:15 +0200

    wrng

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,11 @@ set -x EDITOR hx
 set -x PKG_CONFIG_PATH /usr/local/lib/pkgconfig/lib
 set -x N_PREFIX ~/.local/
 set -x ANDROID_HOME ~/Library/Android/sdk
+set -x EXPO_APPLE_TEAM_ID WG5XTC5GTZ
+set -g fish_function_path $fish_function_path $fisher_path/functions
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home
+
+set -x SKIM_DEFAULT_OPTIONS "--ansi --reverse --color=16 --delimiter=: --preview-window='up:65%:+{2}-/2' --preview='bat --style=numbers --color=always --highlight-line {2}:+0 {1}' --bind='ctrl-d:half-page-down,ctrl-u:half-page-up,?:toggle-preview,alt-/:execute-silent(ql {}),alt-space:execute-silent(ql {})+down,alt-j:preview-down,alt-k:preview-up,alt-h:preview-left,alt-l:preview-right,alt-d:preview-page-down,alt-u:preview-page-up,"
 
 pyenv init - | source
 rbenv init - | source
@@ -44,10 +49,14 @@ function unexpand-home-tilde
     /bin/cat - | string replace $HOME '~'
 end
 
+function anhome
+    /bin/cat - | string replace $HOME /opt/a
+end
 function trim-trailing-slash
     read str
     string replace -r '/$' '' -- $str
 end
+
 function last-download
     echo ~/Downloads/(ls -t ~/Downloads/ | head -1)
 end
@@ -65,6 +74,29 @@ end
 
 
 if status is-interactive
+    function __fish_enable_focus --on-event fish_postexec
+        echo -n \e\[\?1004h
+    end
+    function __fish_disable_focus --on-event fish_preexec
+        echo -n \e\[\?1004l
+    end
     # Commands to run in interactive sessions can go here
     # source ~/config/fish/atuin.fish
+end
+
+# begin -h completion#
+#-h --completion-fish | source
+# end -h completion
+
+# begin wrangler completion
+#wrangler --completion-fish | source
+# end wrangler completion
+
+function __gt_command_completions
+    set -lx SHELL (type -p fish)
+    set -l command (commandline -opc)
+    # uncomment to include options, e.g. -q, --help
+    # $command --get-yargs-completions
+    # uncomment to exclude options (default)
+    $command --get-yargs-completions | string replace -r '\-.*' ''
 end
