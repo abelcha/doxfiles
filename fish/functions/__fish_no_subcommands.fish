9910196815c50245cca072b839bff9b073d5ca6c
Votre Nom <vous@exemple.com>
2025-08-15 03:21:11 +0200

    rmed

--- fish/functions/__fish_no_subcommands.fish
+++
@@ -1,3 +0,0 @@
-function __fish_no_subcommands
-    test -z "$(commandline -opc)[2]"
-end
--- fish/functions/x.fish
+++
@@ -1,7 +0,0 @@
-function x --wraps=xh
-
-    # xh -b --follow $argv 2>/dev/null
-    # if test $status -ne 200
-    xh --follow $argv
-    # end
-end
