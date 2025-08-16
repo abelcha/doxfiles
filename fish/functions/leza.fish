cb65984f7ecea4f31a9ed662f2249115c14931bb
Your Name <you@example.com>
2025-08-16 05:02:34 +0000

    leza

--- fish/functions/leza.fish
+++ fish/functions/leza.fish
@@ -1,10 +1,3 @@
-function leza
-    if type -f -q leza
-        command leza $argv
-    else if type -f -q eza
-        command eza $argv
-    else
-        echo eza not installed
-    end
-    
+function leza --wraps=eza
+    eza --long -F --no-user $argv
 end
--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,15 +1,12 @@
 function ll --wraps=eza
-    #lx $argv
-    #EZA_COLORS="da=38"
     if not type -q eza
         ls -l $argv
         return
-    end 
+    end
     if test -z "$argv[1]"
         timeout --foreground 0.5s eza --long -F --no-user --total-size $argv
-        test $status = 124; and eza --long -F --no-user $argv
+        test $status = 124; and leza $argv
         return 0
     end
-    eza --long -F --no-user --total-size $argv
-    #leza --long -F --no-user --total-size $argv
+    leza --total-size $argv
 end
