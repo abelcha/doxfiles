b993f0c38c1ba677e84a577fa428fdc56d2035aa
Votre Nom <vous@exemple.com>
2025-08-16 06:30:11 +0200

    _

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,11 +1,15 @@
 function ll --wraps=eza
     #lx $argv
     #EZA_COLORS="da=38"
+    if not type -q eza
+        ls -l $argv
+        return
+    end 
     if test -z "$argv[1]"
-        timeout --foreground 0.5s leza --long -F --no-user --total-size $argv
-        test $status = 124; and leza --long -F --no-user $argv
+        timeout --foreground 0.5s eza --long -F --no-user --total-size $argv
+        test $status = 124; and eza --long -F --no-user $argv
         return 0
     end
-    leza --long -F --no-user --total-size $argv
+    eza --long -F --no-user --total-size $argv
     #leza --long -F --no-user --total-size $argv
 end
