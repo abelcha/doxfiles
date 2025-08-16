9a5880052da6ebbcbedeb5985cd9b9f1924d7706
Votre Nom <vous@exemple.com>
2025-08-16 05:45:35 +0200

    _

---
+++ locker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+set --append files "./rclone/rclone.conf" "./fish/conf.d/secrets.fish"
+
+read --prompt-str "pass> " -s -l pass
+
+if [ "$argv[1]" = "encrypt" ]
+    for file in $files
+        7z a -p"$pass" -mhe=on -t7z $file.7z fish/conf.d/secrets.fish $file
+    end
+    # echo "encrypt"
+end
+
+if [ "$argv[1]" = "decrypt" ]
+    for file in $files
+        7z x $file.7z -o"$(path dirname $file)" -p"$pass" -aoa
+    end
+end
