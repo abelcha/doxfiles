8213772627363dac9951c995188a5338a70c882d
Your Name <you@example.com>
2025-08-16 05:13:18 +0000

    _

--- fish/functions/echo2.fish
+++ fish/functions/echo2.fish
@@ -1,3 +1,3 @@
 function echo2
-    echo $argv >&2
+    echo (set_color white --dim )$argv(set_color normal) >&2
 end
--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,9 +1,10 @@
-function ll --wraps=eza
+function ll --wraps=eza --wraps=lla
     if not type -q eza
         ls -l $argv
         return
     end
-    if test -z "$argv[1]"
+    if test -z "$argv[1]"; and test -z "$TOTALSIZE"
+        echo2 fastrack
         timeout --foreground 0.5s eza --long -F --no-user --total-size $argv
         test $status = 124; and leza $argv
         return 0
