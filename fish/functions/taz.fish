6d057a0b9340d1669e76502f653d9415300b5a3e
Your Name <you@example.com>
2025-08-16 09:11:51 +0000

    added

--- fish/functions/taz.fish
+++ fish/functions/taz.fish
@@ -1,6 +1,6 @@
 function taz --wraps=pzstd
     if test -f $argv[1]
-
+        
         zstd --fast --rm $argv[1]
         return
     end
@@ -11,6 +11,11 @@ function taz --wraps=pzstd
     #  tar cf - $argv[1] | pv -s (command gdu -sb $argv[1] | cut -f 1) -N (get_filename $argv[1]).tzst | pzstd $argv[1..] >(get_filename $argv[1]).tzst
     # tcar -c - $argv[1] | pzstd $argv[1..] && trash $argv[1]
     # comp tar.zst $argv && trash $argv
-    aarpack compress --rm $argv
-
+    if type -q aarpack
+        aarpack compress --rm $argv
+    else
+        set p (path normalize "$argv[1]" )
+        ouch compress --fast "$p" "$p.tar.zst" && rm "$p"
+    end
+    
 end
