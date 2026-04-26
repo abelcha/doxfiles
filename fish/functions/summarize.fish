f4e818ff334b3fc830b58d25f2c97071ae98a3e1
abelcha <abel.xz@icloud.com>
2026-04-27 00:56:57 +0200

    feat: remove deprecated duckdb fish shell functions
    Replace with centralized and maintained duckdb
    integration via new CLI tooling and plugins

--- fish/functions/summarize.fish
+++
@@ -1,5 +0,0 @@
-function summarize --wraps=duckdb
-    test -e $argv[1]; and set argv[1] "\"$argv[1]\""
-    set -l cmd "SELECT column_name, column_type, null_percentage as nullpx,approx_unique as a_uniq, round(avg::FLOAT, 2) AS avg, round(std::FLOAT, 2) as avg, q25,  q75, count FROM (SUMMARIZE SELECT * FROM $argv[1] ) $argv[2]"
-    duck $DUCK_DISPLAY $DD -c "$cmd"
-end
